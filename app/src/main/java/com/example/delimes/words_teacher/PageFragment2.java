6fc44dd1306751595d1faf734b977b718c2a6ecc
deLimes <3830123@gmail.com>
2022-08-10 17:20:54 +0300

    deleted finish2

--- app/src/main/java/com/example/delimes/words_teacher/PageFragment2.java
+++ app/src/main/java/com/example/delimes/words_teacher/PageFragment2.java
@@ -400,7 +400,10 @@ public class PageFragment2 extends android.support.v4.app.Fragment {
             Toast.makeText(getActivity().getApplicationContext(), "Finish", Toast.LENGTH_SHORT).show();
             //view.loadUrl("javascript:alert('Prived');");
             //https://context.reverso.net/%D0%BF%D0%B5%D1%80%D0%B5%D0%B2%D0%BE%D0%B4/%D0%B0%D0%BD%D0%B3%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%B8%D0%B9-%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9/pronounce
-            view.loadUrl("javascript:(function(){document.getElementsByClassName('voice2 icon stopped').click();})();");
+
+            //view.loadUrl("javascript:(function(){document.getElementsByClassName('r375lc').click();})();");
+
+            //view.loadUrl("javascript:WebForm_DoPostBackWithOptions( new WebForm_PostBackOptions('r375lc', '', true, '', '', false, true))");
 
 //            view.loadUrl("javascript:(window.onload = function(){" +
 //                    "alert(document.getElementById('UseTranslator'));" +
